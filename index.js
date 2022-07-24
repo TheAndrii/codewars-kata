61ad3b474448a5f7aa775b9a53634771b098d1d3
Andrii Mykolaichyk <106387787+TheAndrii@users.noreply.github.com>
2022-07-24 21:42:29 +0300

    Added new kata for 07.25

--- index.js
+++ index.js
@@ -95,3 +95,35 @@ function past(h, m, s){
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0;
 }
+
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let min = args[0];
+    for (let i = 1; i < args.length; i++) {
+      if (args[i] < min) {
+        min = args[i];
+      }
+    }
+    return min;
+  }
+}
+// another way
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+const circleCircumference = (circle) => Math.PI*2*circle.radius;
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj){
+  const five = [];
+  for (const key in obj) {
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]); 
+  }
+  return five;
+}
